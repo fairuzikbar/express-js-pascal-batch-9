7ca02e728c8f6205ecfb1c84264667d9809def68
Fairuz Ikbar Wikantyo <fairuz.ikbar.w@gmail.com>
2022-08-12 10:24:47 +0700

    [feat] playing with jwt and token

--- src/delivery/middleware/auth.middleware.js
+++ src/delivery/middleware/auth.middleware.js
@@ -1,18 +1,23 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const AuthMiddleware = (req, res, next) => {
     try {
-        const authHeader = req.headers['token'];
+        const authHeader = req.headers['authorization'];
         if(!authHeader){
             return res.status(401).json({
                 "message" : "Unauthorized"
             })
         }
         
-        const token = authHeader
-        if(token !== 'enigma'){
+        const token = authHeader.replace('Bearer ', '');
+        if(!token){
             return res.status(401).json({
                 "message" : "Token Incorrect!"
             })
         }
+        jwt.verify(token, process.env.TOKEN_SECRET, '', null)
         next()
     } catch (error) {
         return res.status(401).json({
--- src/service/authentication.service.js
+++ src/service/authentication.service.js
@@ -1,4 +1,9 @@
+const jwt = require('jsonwebtoken');
+const { s, m } = require('time-convert');
+const dotenv = require('dotenv');
+
 const AuthenticationService = (userService) => {
+    dotenv.config();
     const { findUserByUsernamePassword } = userService
     
     const login = async (payload) => {
@@ -9,7 +14,23 @@ const AuthenticationService = (userService) => {
             );
         if(!user) return `Invalid account!`;
         //TOKEN
-        return `enigma`;
+        //dari const {TOKEN_SECRET,...} sampai di atas return token, baiknya dibuat method sendiri biar clean
+        const { TOKEN_SECRET, TOKEN_EXPIRATION, TOKEN_ALGORITHM } = process.env;
+        const payloadUser = { id : user.id, email : user.email };
+        const algorithm = TOKEN_ALGORITHM;
+        const key = TOKEN_SECRET;
+        const expiresIn = s.from(m)(TOKEN_EXPIRATION);
+        // Urutan: 
+        // (1) payload yang ingin ditampilan
+        // (2) token secret untuk verify signature
+        // (3) { expiresIn } -> Untuk masa hidup token, bisa dari (ms, s, m, h, d) dari npm time-convert
+        // (4) Callback
+        const token = jwt.sign(
+            payloadUser, key,
+            { expiresIn, algorithm },
+            null
+            );
+        return token;
     }
     
     const logout = () => {};
